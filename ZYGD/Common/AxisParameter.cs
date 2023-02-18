df3c2cc2628f7a5bbfa3f4a5c7a905d0516f8e38
unknown <wenkeenna@163.com>
2023-02-18 16:00:55 +0800

    修改DataGrid设置

---
+++ ZYGD/Common/AxisParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZYGD.Common
+{
+    /// <summary>
+    /// 轴参数
+    /// </summary>
+    public class AxisParameter
+    {
+        private string _name;   
+        public string Name { get { return _name; } set { _name = value; } }
+        private int id;
+        public int ID { get { return id; } set { id = value; } }
+        private int speed;
+        public int Speed { get { return speed; } set { speed = value; } }
+        private int acc;
+        public int Acc { get { return acc; } set { acc = value; } }
+        private int dec;
+        public int Dec { get { return dec; } set { dec = value; } }
+
+
+    }
+}
--- ZYGD/ViewModels/ContralCardViewModel.cs
+++ ZYGD/ViewModels/ContralCardViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZYGD.Common;
 
 namespace ZYGD.ViewModels
 {
-    class ContralCardViewModel
+    class ContralCardViewModel : BindableBase
     {
+        public IEventAggregator _eventAggregator;
+        private IDialogService _dialogService;
+        public DelegateCommand<string> btnCommand { get; private set; }
+
+        private ObservableCollection<AxisParameter> axisparas;
+        public ObservableCollection<AxisParameter> AxisParas
+        {
+            get { return axisparas; }
+            set { axisparas = value; RaisePropertyChanged(); }
+        }
+
+        public ContralCardViewModel(IEventAggregator ea, IDialogService dialogService)
+        {
+            _eventAggregator = ea;
+            _dialogService = dialogService;
+            btnCommand = new DelegateCommand<string>(btnFun);
+            AxisParas =new ObservableCollection<AxisParameter>();
+
+            AxisParas.Add(new AxisParameter() { Name = "A1", ID = 001, Speed = 1000, Acc = 100, Dec = 100 });
+            AxisParas.Add(new AxisParameter() { Name = "A2", ID = 001, Speed = 1000, Acc = 100, Dec = 100 });
+            AxisParas.Add(new AxisParameter() { Name = "A3", ID = 001, Speed = 1000, Acc = 100, Dec = 100 });
+            AxisParas.Add(new AxisParameter() { Name = "A4", ID = 001, Speed = 1000, Acc = 100, Dec = 100 });
+
+
+        }
+        private void btnFun(object obj)
+        {
+           
+        }
     }
 }
